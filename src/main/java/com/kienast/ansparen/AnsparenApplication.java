e3a099d98daca6c305fc7aaf0cc0e7ade41c167a
Lukas Kienast <lukaskienast0@gmail.com>
2021-07-22 15:41:28 +0200

    Added jwt verification

--- src/main/java/com/kienast/ansparen/AnsparenApplication.java
+++ src/main/java/com/kienast/ansparen/AnsparenApplication.java
@@ -2,10 +2,16 @@ package com.kienast.ansparen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class AnsparenApplication {
 
+	@Bean
+	public WebClient.Builder getWebClientBuilder() {
+		return WebClient.builder();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AnsparenApplication.class, args);
 	}
--- src/main/java/com/kienast/ansparen/controller/AnsparController.java
+++ src/main/java/com/kienast/ansparen/controller/AnsparController.java
@@ -8,7 +8,9 @@ import java.util.List;
 import javax.validation.Valid;
 
 import com.kienast.ansparen.exception.BadRequestException;
+import com.kienast.ansparen.exception.NotAuthorizedException;
 import com.kienast.ansparen.model.Amount;
+import com.kienast.ansparen.model.TokenVerificationResponse;
 import com.kienast.ansparen.rest.api.AnsparenApi;
 import com.kienast.ansparen.rest.api.model.AnsparEntryModel;
 import com.kienast.ansparen.rest.api.model.CategoryResponseModel;
@@ -19,10 +21,15 @@ import org.apache.logging.log4j.Logger;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import io.swagger.v3.oas.annotations.Operation;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class AnsparController implements AnsparenApi {
@@ -35,13 +42,40 @@ public class AnsparController implements AnsparenApi {
 	@Value("${logging.level.com.kienast.ansparservice}")
 	private String loglevel;
 
+	// Used for WebTemplate
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+
+	@Value("${authURL}")
+	private String authURL;
+
 	@Override
 	@Operation(description = "addEntry")
 	public ResponseEntity<AnsparEntryModel> addEntry(String JWT, String xRequestID, String SOURCE_IP,
 			@Valid AnsparEntryModel ansparEntryModel) {
 
-		initializeLogInfo(xRequestID, SOURCE_IP, "1");
+		initializeLogInfo(xRequestID, SOURCE_IP, "");
 		logger.info("Got Request (Add Anspar Entry) for " + ansparEntryModel.getDescription());
+
+		try {
+			TokenVerificationResponse tokenResponse = new TokenVerificationResponse();
+
+			logger.info("Call Authentication Microservice for JWT Verification");
+			tokenResponse = webClientBuilder.build().post() // RequestMethod
+					.uri(authURL + "/jwt").header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).header("JWT", JWT)
+					.header("X-Request-ID", xRequestID).header("SOURCE_IP", SOURCE_IP).retrieve() // run command
+					.onStatus(HttpStatus::is4xxClientError, response -> {
+						return Mono.error(new NotAuthorizedException(String.format("Failed JWT Verification")));
+					}).bodyToMono(TokenVerificationResponse.class) // convert Response
+					.block(); // do as Synchronous call
+
+			initializeLogInfo(xRequestID, SOURCE_IP, tokenResponse.getUserId());
+			logger.info("Added userId to log");
+		} catch (Exception e) {
+
+			logger.error("Error on verifiying jwt" + e.getMessage());
+			throw new BadRequestException("Verification Failure");
+		}
 
 		Amount entity = null;
 		AnsparEntryModel response = new AnsparEntryModel();
@@ -66,32 +100,76 @@ public class AnsparController implements AnsparenApi {
 	}
 
 	@Override
-	@Operation(description = "get Entry by categoryName")
+	@Operation(description = "get Entries")
+	
 	public ResponseEntity<List<CategoryResponseModel>> getCategories(String JWT, String xRequestID, String SOURCE_IP) {
 
-		initializeLogInfo(xRequestID, SOURCE_IP, "1");
+		List<CategoryResponseModel> categoryResponse = null;
+
+		initializeLogInfo(xRequestID, SOURCE_IP, "");
 		logger.info("Got Request (Get Entries)");
 
-		List<CategoryResponseModel> response = amountService.getAmounts();
+		try {
+			TokenVerificationResponse tokenResponse = new TokenVerificationResponse();
+
+			logger.info("Call Authentication Microservice for JWT Verification");
+			tokenResponse = webClientBuilder.build().post() // RequestMethod
+					.uri(authURL + "/jwt").header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).header("JWT", JWT)
+					.header("X-Request-ID", xRequestID).header("SOURCE_IP", SOURCE_IP).retrieve() // run command
+					.onStatus(HttpStatus::is4xxClientError, response -> {
+						return Mono.error(new NotAuthorizedException(String.format("Failed JWT Verification")));
+					}).bodyToMono(TokenVerificationResponse.class) // convert Response
+					.block(); // do as Synchronous call
+
+			initializeLogInfo(xRequestID, SOURCE_IP, tokenResponse.getUserId());
+			logger.info("Added userId to log");
+
+			categoryResponse = amountService.getAmounts();
+
+		} catch (Exception e) {
+
+			logger.error("Error on verifiying jwt" + e.getMessage());
+		}
 
 		logger.info("Successfully loaded entries");
 
-		return ResponseEntity.ok(response);
+		return ResponseEntity.ok(categoryResponse);
 	}
 
 	@Override
-	@Operation(description = "get Entries")
+	@Operation(description = "get Entry by categoryName")
 	public ResponseEntity<CategoryResponseModel> getCategory(String description, String JWT, String xRequestID,
 			String SOURCE_IP) {
 
-		initializeLogInfo(xRequestID, SOURCE_IP, "1");
+		CategoryResponseModel categoryResponse = null;
+		initializeLogInfo(xRequestID, SOURCE_IP, "");
 		logger.info("Got Request (Get Entry) for name " + description);
 
-		CategoryResponseModel response = amountService.getAmountsByCategory(description);
+		try {
+			TokenVerificationResponse tokenResponse = new TokenVerificationResponse();
+
+			logger.info("Call Authentication Microservice for JWT Verification");
+			tokenResponse = webClientBuilder.build().post() // RequestMethod
+					.uri(authURL + "/jwt").header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).header("JWT", JWT)
+					.header("X-Request-ID", xRequestID).header("SOURCE_IP", SOURCE_IP).retrieve() // run command
+					.onStatus(HttpStatus::is4xxClientError, response -> {
+						return Mono.error(new NotAuthorizedException(String.format("Failed JWT Verification")));
+					}).bodyToMono(TokenVerificationResponse.class) // convert Response
+					.block(); // do as Synchronous call
+
+			initializeLogInfo(xRequestID, SOURCE_IP, tokenResponse.getUserId());
+			logger.info("Added userId to log");
+
+			categoryResponse = amountService.getAmountsByCategory(description);
+
+		} catch (Exception e) {
+
+			logger.error("Error on verifiying jwt " + e.getMessage());
+		}
 
 		logger.info("Successfully loaded entry on " + description);
 
-		return ResponseEntity.ok(response);
+		return ResponseEntity.ok(categoryResponse);
 	}
 
 	private void initializeLogInfo(String requestId, String sourceIP, String userId) {
